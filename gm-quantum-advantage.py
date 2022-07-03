064f457aecaf8a691c76438414c8339c9e0c8adf
mi2valley <monocoin189@gmail.com>
2022-07-03 14:09:21 +0900

    fix

--- gm-quantum-advantage.py
+++ gm-quantum-advantage.py
@@ -5,27 +5,48 @@ import numpy as np
 import datetime as dt
 
 if(dt.date.today().weekday() == 4 or 5 or 6):
-    eng = sf.RemoteEngine("simulon_gaussian")
+    prog = gbs_tdm()
+
+    compile_options = {
+        "device": device,
+        "realistic_loss": True,
+    }
+
+    run_options = {
+        "shots": None,
+        "crop": True,
+        "space_unroll": True,
+    }
+
+    eng_sim = sf.Engine(backend="gaussian")
+    results_sim = eng_sim.run(prog, **run_options, compile_options=compile_options)
+
+
+
 else:
+    prog = gbs_tdm()
+
+    shots = 10_000
+    results = eng.run(prog, shots=shots, crop=True)
+    print(results.samples)
+
+def gbs_tdm():
     eng = sf.RemoteEngine("borealis")
+    device = eng.device
 
-device = eng.device
+    gate_args_list = borealis_gbs(device, modes=216, squeezing="high")
+    delays = [1, 6, 36]
+    n, N = get_mode_indices(delays)
 
-gate_args_list = borealis_gbs(device, modes=216, squeezing="high")
-delays = [1, 6, 36]
-n, N = get_mode_indices(delays)
+    from strawberryfields.ops import Sgate, Rgate, BSgate, MeasureFock
 
-from strawberryfields.ops import Sgate, Rgate, BSgate, MeasureFock
+    prog = sf.TDMProgram(N)
 
-prog = sf.TDMProgram(N)
+    with prog.context(*gate_args_list) as (p, q):
+        Sgate(p[0]) | q[n[0]]
+        for i in range(len(delays)):
+            Rgate(p[2 * i + 1]) | q[n[i]]
+            BSgate(p[2 * i + 2], np.pi / 2) | (q[n[i + 1]], q[n[i]])
+        MeasureFock() | q[0]
 
-with prog.context(*gate_args_list) as (p, q):
-    Sgate(p[0]) | q[n[0]]
-    for i in range(len(delays)):
-        Rgate(p[2 * i + 1]) | q[n[i]]
-        BSgate(p[2 * i + 2], np.pi / 2) | (q[n[i + 1]], q[n[i]])
-    MeasureFock() | q[0]
-
-shots = 10_000
-results = eng.run(prog, shots=shots, crop=True)
-print(results.samples)
+    return prog
